baaa4fcdf7a58f9f1fa31aeb4d7541253f350345
Abdul Hadi Alaidi <aalaidi@my.bridgeport.edu>
2026-04-30 07:24:47 +0300

    fix(theme): adjust import paths and extend component constructors
    Update community and collection search result list element components to:
    - Correct relative import paths after restructuring
    - Add missing constructor with required dependencies (TruncatableService, DSONameService, AppConfig)
    - Import APP_CONFIG and AppConfig for configuration access
    - Ensure components properly inherit from parent SearchResultListElementComponent

--- src/themes/uowasit/app/shared/object-list/search-result-list-element/collection-search-result/collection-search-result-list-element.component.ts
+++ src/themes/uowasit/app/shared/object-list/search-result-list-element/collection-search-result/collection-search-result-list-element.component.ts
@@ -1,26 +1,32 @@
+import {
+  APP_CONFIG,
+  AppConfig,
+} from '@dspace/config/app-config.interface';
 import { NgClass } from '@angular/common';
 import {
   Component,
   OnInit,
+  Inject,
 } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Collection } from '@dspace/core/shared/collection.model';
 import { CollectionSearchResult } from '@dspace/core/shared/object-collection/collection-search-result.model';
 import { ViewMode } from '@dspace/core/shared/view-mode.model';
 
-import { DSONameService } from '../../../../../../../../app/core/breadcrumbs/dso-name.service';
-import { ThemedBadgesComponent } from '../../../../../../../../app/shared/object-collection/shared/badges/themed-badges.component';
-import { listableObjectComponent } from '../../../../../../../../app/shared/object-collection/shared/listable-object/listable-object.decorator';
-import { SearchResultListElementComponent } from '../../../../../../../../app/shared/object-list/search-result-list-element/search-result-list-element.component';
-import { LocaleAwareDSONameService } from '../../../locale-aware-dso-name.service';
+import { DSONameService } from '../../../../../../../app/core/breadcrumbs/dso-name.service';
+import { ThemedBadgesComponent } from '../../../../../../../app/shared/object-collection/shared/badges/themed-badges.component';
+import { listableObjectComponent } from '../../../../../../../app/shared/object-collection/shared/listable-object/listable-object.decorator';
+import { SearchResultListElementComponent } from '../../../../../../../app/shared/object-list/search-result-list-element/search-result-list-element.component';
+import { LocaleAwareDSONameService } from '../../../../shared/locale-aware-dso-name.service';
+import { TruncatableService } from '../../../../../../../app/shared/truncatable/truncatable.service';
 
 @Component({
   selector: 'ds-collection-search-result-list-element',
   styleUrls: [
-    '../../../../../../../../app/shared/object-list/search-result-list-element/search-result-list-element.component.scss',
-    '../../../../../../../../app/shared/object-list/search-result-list-element/collection-search-result/collection-search-result-list-element.component.scss',
+    '../../../../../../../app/shared/object-list/search-result-list-element/search-result-list-element.component.scss',
+    '../../../../../../../app/shared/object-list/search-result-list-element/collection-search-result/collection-search-result-list-element.component.scss',
   ],
-  templateUrl: '../../../../../../../../app/shared/object-list/search-result-list-element/collection-search-result/collection-search-result-list-element.component.html',
+  templateUrl: '../../../../../../../app/shared/object-list/search-result-list-element/collection-search-result/collection-search-result-list-element.component.html',
   imports: [
     NgClass,
     RouterLink,
@@ -33,6 +39,14 @@ import { LocaleAwareDSONameService } from '../../../locale-aware-dso-name.servic
 @listableObjectComponent(CollectionSearchResult, ViewMode.ListElement, undefined, 'custom')
 export class CollectionSearchResultListElementComponent extends SearchResultListElementComponent<CollectionSearchResult, Collection> implements OnInit {
   showThumbnails: boolean;
+
+  constructor(
+    protected truncatableService: TruncatableService,
+    public dsoNameService: DSONameService,
+    @Inject(APP_CONFIG) protected appConfig: AppConfig,
+  ) {
+    super(truncatableService, dsoNameService, appConfig);
+  }
 
   ngOnInit(): void {
     super.ngOnInit();
--- src/themes/uowasit/app/shared/object-list/search-result-list-element/community-search-result/community-search-result-list-element.component.ts
+++ src/themes/uowasit/app/shared/object-list/search-result-list-element/community-search-result/community-search-result-list-element.component.ts
@@ -1,26 +1,32 @@
+import {
+  APP_CONFIG,
+  AppConfig,
+} from '@dspace/config/app-config.interface';
 import { NgClass } from '@angular/common';
 import {
   Component,
   OnInit,
+  Inject,
 } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Community } from '@dspace/core/shared/community.model';
 import { CommunitySearchResult } from '@dspace/core/shared/object-collection/community-search-result.model';
 import { ViewMode } from '@dspace/core/shared/view-mode.model';
 
-import { DSONameService } from '../../../../../../../../app/core/breadcrumbs/dso-name.service';
-import { ThemedBadgesComponent } from '../../../../../../../../app/shared/object-collection/shared/badges/themed-badges.component';
-import { listableObjectComponent } from '../../../../../../../../app/shared/object-collection/shared/listable-object/listable-object.decorator';
-import { SearchResultListElementComponent } from '../../../../../../../../app/shared/object-list/search-result-list-element/search-result-list-element.component';
-import { LocaleAwareDSONameService } from '../../../locale-aware-dso-name.service';
+import { DSONameService } from '../../../../../../../app/core/breadcrumbs/dso-name.service';
+import { ThemedBadgesComponent } from '../../../../../../../app/shared/object-collection/shared/badges/themed-badges.component';
+import { listableObjectComponent } from '../../../../../../../app/shared/object-collection/shared/listable-object/listable-object.decorator';
+import { SearchResultListElementComponent } from '../../../../../../../app/shared/object-list/search-result-list-element/search-result-list-element.component';
+import { LocaleAwareDSONameService } from '../../../../shared/locale-aware-dso-name.service';
+import { TruncatableService } from '../../../../../../../app/shared/truncatable/truncatable.service';
 
 @Component({
   selector: 'ds-community-search-result-list-element',
   styleUrls: [
-    '../../../../../../../../app/shared/object-list/search-result-list-element/search-result-list-element.component.scss',
-    '../../../../../../../../app/shared/object-list/search-result-list-element/community-search-result/community-search-result-list-element.component.scss',
+    '../../../../../../../app/shared/object-list/search-result-list-element/search-result-list-element.component.scss',
+    '../../../../../../../app/shared/object-list/search-result-list-element/community-search-result/community-search-result-list-element.component.scss',
   ],
-  templateUrl: '../../../../../../../../app/shared/object-list/search-result-list-element/community-search-result/community-search-result-list-element.component.html',
+  templateUrl: '../../../../../../../app/shared/object-list/search-result-list-element/community-search-result/community-search-result-list-element.component.html',
   imports: [
     NgClass,
     RouterLink,
@@ -33,6 +39,14 @@ import { LocaleAwareDSONameService } from '../../../locale-aware-dso-name.servic
 @listableObjectComponent(CommunitySearchResult, ViewMode.ListElement, undefined, 'custom')
 export class CommunitySearchResultListElementComponent extends SearchResultListElementComponent<CommunitySearchResult, Community> implements OnInit {
   showThumbnails: boolean;
+
+  constructor(
+    protected truncatableService: TruncatableService,
+    public dsoNameService: DSONameService,
+    @Inject(APP_CONFIG) protected appConfig: AppConfig,
+  ) {
+    super(truncatableService, dsoNameService, appConfig);
+  }
 
   ngOnInit(): void {
     super.ngOnInit();
