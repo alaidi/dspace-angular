0bddaaa6a1dde88fda0f14b3828a0bd901c02ab8
Abdul Hadi Alaidi <aalaidi@my.bridgeport.edu>
2026-07-05 09:21:00 +0300

    Refactor code structure for improved readability and maintainability

--- src/themes/uowasit/app/header/header.component.ts
+++ src/themes/uowasit/app/header/header.component.ts
@@ -2,15 +2,17 @@ import { AsyncPipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { ThemedLangSwitchComponent } from 'src/app/shared/lang-switch/themed-lang-switch.component';
 
 import { ContextHelpToggleComponent } from '../../../../app/header/context-help-toggle/context-help-toggle.component';
 import { HeaderComponent as BaseComponent } from '../../../../app/header/header.component';
 import { ThemedNavbarComponent } from '../../../../app/navbar/themed-navbar.component';
 import { ThemedSearchNavbarComponent } from '../../../../app/search-navbar/themed-search-navbar.component';
-import { ThemedAuthNavMenuComponent } from '../../../../app/shared/auth-nav-menu/themed-auth-nav-menu.component';
+import { HostWindowService } from '../../../../app/shared/host-window.service';
 import { ImpersonateNavbarComponent } from '../../../../app/shared/impersonate-navbar/impersonate-navbar.component';
+import { ThemedAuthNavMenuComponent } from '../../../../app/shared/auth-nav-menu/themed-auth-nav-menu.component';
+import { MenuService } from '../../../../app/shared/menu/menu.service';
 
 @Component({
   selector: 'ds-themed-header',
@@ -30,4 +32,17 @@ import { ImpersonateNavbarComponent } from '../../../../app/shared/impersonate-n
   ],
 })
 export class HeaderComponent extends BaseComponent {
+  constructor(
+    protected menuService: MenuService,
+    protected windowService: HostWindowService,
+    protected translate: TranslateService,
+  ) {
+    super(menuService, windowService);
+  }
+
+  get logoSrc(): string {
+    return this.translate.currentLang === 'ar'
+      ? 'assets/uowasit/images/logo-uowasit.png'
+      : 'assets/uowasit/images/logo.png';
+  }
 }
