697c7d8501bb1ebf7ca558ec5fcf2858f13d1998
Abdul Hadi Alaidi <aalaidi@my.bridgeport.edu>
2026-07-09 18:51:18 +0300

    feat: redesign search page for items-only results with refined styles and layout

--- src/themes/uowasit/app/search-page/search-page.component.ts
+++ src/themes/uowasit/app/search-page/search-page.component.ts
@@ -7,9 +7,8 @@ import { ThemedSearchComponent } from '../../../../app/shared/search/themed-sear
 
 @Component({
   selector: 'ds-themed-search-page',
-  // styleUrls: ['./search-page.component.scss'],
-  // templateUrl: './search-page.component.html'
-  templateUrl: '../../../../app/search-page/search-page.component.html',
+  // uowasit: own template so the main /search is scoped to items (dsoType=item)
+  templateUrl: './search-page.component.html',
   providers: [
     {
       provide: SEARCH_CONFIG_SERVICE,
